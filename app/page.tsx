f30a10f2228898a78909a996618c632d74d10431
toycrane <alwaysfun2183@gmail.com>
2023-06-30 00:17:59 +0900

    feat: spinner 추가

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Spinner } from "@/components/ui/spinner";
+
+import imglyRemoveBackground from "@imgly/background-removal";
 
 import { ChangeEvent, useState, MouseEvent, useEffect } from "react";
 import Image from "next/image";
@@ -11,6 +13,7 @@ import Image from "next/image";
 export default function Home() {
   const [selectedFile, setSelectedFile] = useState<File>();
   const [previewSrc, setPreviewSrc] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     if (selectedFile) {
@@ -31,13 +34,26 @@ export default function Home() {
   };
 
   const fileUploadHandler = (event: MouseEvent<HTMLButtonElement>) => {
+    setLoading(true);
     event.preventDefault();
     if (selectedFile) {
-      const formData = new FormData();
-      formData.append("file", selectedFile);
-      console.log(formData);
+      imglyRemoveBackground(selectedFile, {
+        publicPath: "/",
+        debug: true,
+        model: "medium",
+        proxyToWorker: true,
+        fetchArgs: {
+          mode: "no-cors",
+        },
+      }).then((blob: Blob) => {
+        // The result is a blob encoded as PNG. It can be converted to an URL to be used as HTMLImage.src
+        const url = URL.createObjectURL(blob);
+        setPreviewSrc(url);
+        setLoading(false);
+      });
     } else {
       console.log("No file selected");
+      setLoading(false);
     }
   };
 
@@ -59,11 +75,12 @@ export default function Home() {
         )}
       </div>
       <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="picture">Picture</Label>
+        <Label htmlFor="picture">배경을 제거할 이미지를 업로드 해주세요.</Label>
         <Input id="picture" type="file" onChange={fileSelectedHandler} />
         <Button type="submit" onClick={fileUploadHandler}>
           배경 제거하기
         </Button>
+        {loading && <Spinner />}
       </div>
     </main>
   );
---
+++ components/ui/spinner.tsx
@@ -0,0 +1,17 @@
+const Spinner = () => (
+  <div
+    id="loading"
+    className="fixed inset-0 z-50 flex items-center justify-center h-100vh bg-transparent"
+  >
+    <div
+      className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status"
+    >
+      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+        Loading...
+      </span>
+    </div>
+  </div>
+);
+
+export { Spinner };
