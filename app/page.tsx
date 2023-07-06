ceaf33e8468c88d06103986b491490a7e9062579
toycrane <alwaysfun2183@gmail.com>
2023-07-06 22:52:05 +0900

    feat: error Alert 추가

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,20 @@ import { ChangeEvent, useState, MouseEvent, useEffect, useRef } from "react";
 import Image from "next/image";
 import { Upload } from "lucide-react";
 
+import { AlertCircle } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+export function AlertDestructive({ error }: { error: string }) {
+  return (
+    <Alert variant="destructive" className="mb-4">
+      <AlertCircle className="h-4 w-4" />
+      <AlertTitle>Error</AlertTitle>
+      <AlertDescription>{error}</AlertDescription>
+    </Alert>
+  );
+}
+
 export default function Home() {
   const [selectedFile, setSelectedFile] = useState<File>();
   const [removedImage, setRemovedImage] = useState<{
@@ -19,6 +33,7 @@ export default function Home() {
   const [loading, setLoading] = useState<boolean>(false);
   const [currentImageSrc, setCurrentImageSrc] = useState<string>("");
   const [quality, setQuality] = useState<"small" | "medium">("small");
+  const [error, setError] = useState<string | undefined>();
   const hiddenInputRef = useRef<HTMLInputElement>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
@@ -71,7 +86,7 @@ export default function Home() {
         setLoading(false);
       });
     } else {
-      console.log("No file selected");
+      setError("이미지를 선택해주세요");
       setLoading(false);
     }
   };
@@ -101,10 +116,10 @@ export default function Home() {
         setCurrentImageSrc("");
         setSelectedFile(item.getAsFile() as File);
       } else {
-        console.log("Only PNG and JPEG images are supported.");
+        setError("이미지는 PNG 및 JPEG 만 지원됩니다.");
       }
     } else {
-      console.log("Only PNG and JPEG images are supported.");
+      setError("이미지는 PNG 및 JPEG 만 지원됩니다.");
     }
   };
 
@@ -169,39 +184,49 @@ export default function Home() {
           )}
         </AspectRatio>
       </div>
-      <div className="mb-4">
-        <Tabs
-          defaultValue="small"
-          className="w-[400px]"
-          onValueChange={(value) => setQuality(value as "small" | "medium")}
-        >
-          <TabsList>
-            <TabsTrigger value="small">저화질</TabsTrigger>
-            <TabsTrigger value="medium">고화질</TabsTrigger>
-          </TabsList>
-        </Tabs>
-      </div>
+      {error ? (
+        <AlertDestructive error={"이미지는 PNG 및 JPEG 만 지원됩니다."} />
+      ) : (
+        <>
+          <div className="mb-4">
+            <Tabs
+              defaultValue="small"
+              className="w-[400px]"
+              onValueChange={(value) => setQuality(value as "small" | "medium")}
+            >
+              <TabsList>
+                <TabsTrigger value="small">저화질</TabsTrigger>
+                <TabsTrigger value="medium">고화질</TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </div>
 
-      <div className="grid w-full items-center gap-1.5">
-        {!removedImage?.src &&
-          (loading ? (
-            <LoadingButton />
-          ) : (
-            <Button type="submit" onClick={fileUploadHandler}>
-              배경 제거하기
-            </Button>
-          ))}
-        {removedImage?.src && (
-          <>
-            <Button onClick={handleDownload} className="w-full">
-              이미지 다운로드
-            </Button>
-            <Button variant="outline" onClick={handleReset} className="w-full">
-              다른 이미지 업로드하기
-            </Button>
-          </>
-        )}
-      </div>
+          <div className="grid w-full items-center gap-1.5">
+            {!removedImage?.src &&
+              (loading ? (
+                <LoadingButton />
+              ) : (
+                <Button type="submit" onClick={fileUploadHandler}>
+                  배경 제거하기
+                </Button>
+              ))}
+            {removedImage?.src && (
+              <>
+                <Button onClick={handleDownload} className="w-full">
+                  이미지 다운로드
+                </Button>
+                <Button
+                  variant="outline"
+                  onClick={handleReset}
+                  className="w-full"
+                >
+                  다른 이미지 업로드하기
+                </Button>
+              </>
+            )}
+          </div>
+        </>
+      )}
     </main>
   );
 }
