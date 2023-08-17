5daeaad8ee4891aa2b57ca74139d0f71134f7e5e
toycrane <alwaysfun2183@gmail.com>
2023-08-17 20:41:51 +0900

    feat: og:image 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,56 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import SiteHeader from "@/components/site-header";
+import { siteConfig } from "@/config/site";
+import { Metadata } from "next";
 
-export const metadata = {
-  title: "배경화면 깔끔하게 지우기",
-  description: "배경화면을 깔끔하게 지우고, 투명 배경으로 만들어보세요!",
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: ["배경화면 지우기", "배경화면", "누끼따기"],
+  authors: [
+    {
+      name: siteConfig.author,
+      url: siteConfig.links.blog,
+    },
+  ],
+  creator: siteConfig.author,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@alwaysfun2183",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-icon-180x180.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,9 @@
 export const siteConfig = {
-  name: "nomoreBG",
-  url: "https://www.nomoreBG.com",
-  ogImage: "https://ui.shadcn.com/logo.svg",
+  name: "배경화면 깔끔하게 지우기",
   description: "클릭 한 번으로 사진에서 배경화면을 제거하세요!",
+  url: "https://www.nomorebg.com",
+  ogImage: "https://www.nomorebg.com/og.png",
+  author: "toy-crane",
   links: {
     twitter: "https://twitter.com/alwaysfun2183",
     github: "https://github.com/toy-crane",
